8737267af51566f0e547f95afc65f48ab5f40c3e
Rishav <kuchiyarishav6@gmail.com>
2022-09-22 14:03:31 +0530

    Changed

--- LinearSearch.java/Code.java
+++ LinearSearch.java/Code.java
@@ -1,17 +1,14 @@
-public class Code {
+public class Code{
     public static void main(String[] args) {
-        int nums[]={1,2,3,4,5,6,7,8,9};
-        System.out.println(LineraSearch(nums, 5));
+        int [] num ={1,2,3,4,5,6,7,8};
+        int ans = LinerarSearch(num, 5);
+        System.out.println(ans);
     }
-
-    // Search in the Array
-    // Return the function if Found Else return -1
-  
-    static int LineraSearch(int [] arr, int target){
-        for(int index=0;index<arr.length;index++){
-            if(arr[index]==target){
-                return index;
-            }else return-1;
-        }
+    static int LinerarSearch(int arr[], int target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                return i;
+            }
+        } return -1;
     }
 }
