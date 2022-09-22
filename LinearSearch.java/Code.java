6e66489f6dea14bee555f0f56893a67dcd366659
Rishav <kuchiyarishav6@gmail.com>
2022-09-22 14:13:53 +0530

    Chnage in target

--- LinearSearch.java/Code.java
+++ LinearSearch.java/Code.java
@@ -1,7 +1,7 @@
 public class Code{
     public static void main(String[] args) {
         int [] num ={1,2,3,4,5,6,7,8};
-        int ans = LinerarSearch(num, 5);
+        int ans = LinerarSearch(num, 15);
         System.out.println(ans);
     }
     static int LinerarSearch(int arr[], int target){
